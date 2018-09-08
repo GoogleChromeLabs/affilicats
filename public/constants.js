6b3e4cbe0e7a049b4548846dc379e92906bafb64
Glitch (affilicats) <none>
2018-09-08 13:31:50 +0000

    Initial commit

---
+++ public/constants.js
@@ -0,0 +1,5 @@
+const ICON_IMG_URL = 'https://cdn.glitch.com/8194e9d5-f0a3-4d19-9253-7797a0366c1c%2Fcat.png?1536335780609';
+const CAT_IMG_URL = 'https://cdn.glitch.com/8194e9d5-f0a3-4d19-9253-7797a0366c1c%2Fcat.svg?1536335866054';
+const MAP_IMG_URL = 'https://cdn.glitch.com/8194e9d5-f0a3-4d19-9253-7797a0366c1c%2Fmap.svg?1535631025878';
+const OFFLINE_IMG_URL = 'https://cdn.glitch.com/8194e9d5-f0a3-4d19-9253-7797a0366c1c%2Foffline.svg?1536061339974';
+const TIMEOUT_IMG_URL = 'https://cdn.glitch.com/8194e9d5-f0a3-4d19-9253-7797a0366c1c%2Fclock.svg?1536336180098';
