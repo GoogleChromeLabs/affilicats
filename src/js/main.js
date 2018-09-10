a5d6bfaacece51614fc94cc7f6c99e32e2502a72
tomayac <steiner.thomas@gmail.com>
2018-09-10 21:26:23 +0200

    Smaller map

--- src/js/main.js
+++ src/js/main.js
@@ -115,7 +115,7 @@ const renderCats = (data, template, main) => {
         Math.random() * 180 * 100000) / 100000;
     map.dataset.src = `https://maps.googleapis.com/maps/api/staticmap
         ?autoscale=1
-        &size=500x400
+        &size=500x350
         &maptype=terrain
         &key=AIzaSyBWZnCRi6oar3MTjR0HkR1lK52_mTe0Rks
         &format=png
